2c201dca9185fd3b2a229b083e1193d032d08ad0
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-09-23 13:30:46 -0700

    changed the name data

--- Bankist-app-Arrays/script.js
+++ Bankist-app-Arrays/script.js
@@ -4,7 +4,7 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Kester Davis',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
