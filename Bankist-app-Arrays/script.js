bfe3fa3bcecf8281cf06be5b2d561accd4448f71
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-09-25 08:04:25 -0700

    coding challenge

--- Bankist-app-Arrays/script.js
+++ Bankist-app-Arrays/script.js
@@ -77,8 +77,16 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-
 displayMovements(account1.movements);
+
+const calDisplayBalance = function (movements) {
+  const balance = movements.reduce(
+    (accumulator, current) => accumulator + current,
+    0
+  );
+  labelBalance.textContent = `${balance}€`;
+};
+calDisplayBalance(account1.movements);
 
 // Computing Usernames
 // username consist of the first alphabet of each usernames
@@ -91,9 +99,7 @@ const createUserNames = function (accs) {
       .join('');
   });
 };
-
 createUserNames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -317,7 +323,7 @@ GOOD LUCK 😀
 
 // console.log(movementsDiscription);
 
-console.log(movements);
+// console.log(movements);
 // // Filter method
 // const deposits = movements.filter(mov => mov > 0);
 // console.log(deposits);
@@ -334,9 +340,41 @@ console.log(movements);
 
 // Reduce method
 // Accumulator -> SNOWBALL
-const balance = movements.reduce(function (accumulator, current, i, arr) {
-  console.log(`Iteration ${i} : ${accumulator} `);
-  return accumulator + current;
-}, 0); // Setting the accumulator to 0
+// const balance = movements.reduce(function (accumulator, current, i, arr) {
+//   console.log(`Iteration ${i} : ${accumulator} `);
+//   return accumulator + current;
+// }, 0); // Setting the accumulator to 0
 
-console.log(balance);
+// console.log(balance);
+
+//doing same thing with a for of loop
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
+
+// console.log(balance2);
+
+// more on reduce
+//get the max in the movements array
+console.log(movements);
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
