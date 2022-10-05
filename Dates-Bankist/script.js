16628bd9f9fb344c836d4aae6f664231998c47de
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-10-05 03:06:39 -0700

    pratice on maths libs and some more functions

--- Dates-Bankist/script.js
+++ Dates-Bankist/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,100 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+// console.log(23 === 23.0);
+//! Base 10 :- 0 to 9
+//! Binary 2 :-  0 to 1
+// console.log(0.1 + 0.2); //* result is meant to be - 0.3 ,but in js its - 0.30000000000000004
+// console.log(0.1 + 0.2 === 0.3); //* returns false when its mean to be true;
+
+//! Conversion
+// console.log(+23);
+// console.log(+'23');
+
+//! Parsing
+// console.log(Number.parseInt('30px', 10)); // Base 10
+// console.log(Number.parseInt('e23')); // the first value should always bea number else it returns NaN
+// console.log(Number.parseInt(2.5)); // if int is used for float it returns the first value
+// console.log(Number.parseFloat(2.5));
+
+//! Check if value is not a number
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN(+'20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
+
+//! Check if value is a number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
+
+//! Check is value is Integer
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23.5));
+// console.log(Number.isInteger(23 / 0));
+
+//
+// ! MATHS AND ROUNDING
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+// console.log(Math.max(20, 1, 3, 4, 23, 56, 18));
+// console.log(Math.max(20, 1, 3, 4, 23, '56', 18)); // it also convert type
+// console.log(Math.max(20, 1, 3, 4, 23, '56px', 18));
+// console.log(Math.min(20, 1, 3, 4, 23, 56, 18));
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+/*
+ 
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(5, 10));
+
+//! Rounding Integers
+console.log(Math.trunc(23.4));
+
+console.log(Math.round(23.4));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.9));
+
+//! Rounding decimals
+//* NOTE:  to Fixed rerturns str
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3)); // to 3 decimals place
+console.log((2.735).toFixed(2));
+
+//* Convert to number
+console.log(+(2.735).toFixed(2));
+
+
+// % modulos
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+// result * div + result
+console.log(8 / 3); // 8 = 2 * 3 + 2
+// a function that returns true if a num isEven
+const isEven = num => num % 2 === 0;
+console.log(isEven(23));
+console.log(isEven(20));
+console.log(isEven(12));
+
+labelBalance.addEventListener('click', () => {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'grey';
+    }
+  });
+});
+*/
