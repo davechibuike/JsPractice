7c7962fc2496ebece16cb0e93e631e7765f1657b
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-10-17 08:53:07 -0700

    advance dom

--- bankist-marketing-advDom/script.js
+++ bankist-marketing-advDom/script.js
@@ -28,3 +28,44 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+const btns = document.getElementsByClassName('btn');
+console.log(btns);
+
+//! creating and inserting elements
+//  insertAdjecentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionalities and analytics.';
+
+message.innerHTML =
+  'We use cookies for improved functionalities and analytics. <buttton class="btn btn--close--cookie">Got it!</button>';
+console.log(message);
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// ! Detele elements
+document.querySelector('.btn--close--cookie').addEventListener('click', () => {
+  message.remove(); // recent method
+  // message.parentElement.removeChild(message); // old method
+});
