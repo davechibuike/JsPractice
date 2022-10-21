7f3400ffdfa439cd55c8be1b56b9f49ce5edf80a
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-10-20 17:58:04 -0700

    sticky nav bar at secction 1

--- bankist-marketing-advDom/script.js
+++ bankist-marketing-advDom/script.js
@@ -136,6 +136,14 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+//! Sticky Nav
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //
 //////////////////////////////////////////////////////////////////////
 //! Practice Session
