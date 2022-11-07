1e48689e5f21a2d4be94ed306017fb2e6e68dc44
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-11-07 06:45:26 -0800

    constructor functions and protypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -6,23 +6,41 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-  //   Never do this
+  //   Never do this, Use prototype rather
   //   this.calcAge = function () {
   //     console.log(2022 - this.birthYear);
   //   };
 };
 
-const dave = new Person('Dave', 1995);
-console.log(dave);
+const dave = new Person('dave', 1995);
 // 1. New {} is created
 // 2. func is called, this = {}
 // 3. {} is linked to prototype
 // 4. function automatically returns {}
 
-const matilda = new Person('Matilda', 2010);
-const becham = new Person('Becham', 1992);
-console.log(matilda, becham);
+const matilda = new Person('matilda', 2010);
+const becham = new Person('becham', 1992);
+// console.log(dave, matilda, becham);
 
-const jay = 'jay';
-console.log(dave instanceof Person);
-console.log(jay instanceof Person);
+// const jay = 'jay';
+// console.log(dave instanceof Person);
+// console.log(jay instanceof Person);
+
+//* Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+Person.prototype.upperFirstName = function () {
+  let fName =
+    this.firstName.split('')[0].toUpperCase() + this.firstName.slice(1);
+  console.log(fName);
+};
+
+//* Using prototypes
+// dave.calcAge();
+// dave.upperFirstName();
+
+// * View all prototypes
+console.log(dave.__proto__);
