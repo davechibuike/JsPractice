0195d50f2958efe6aabd7032a83b882763c247d6
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-11-07 06:13:26 -0800

    const Js = function(constructor, functions){ }

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// constructor function
+const Person = function (firstName, birthYear) {
+  // Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   Never do this
+  //   this.calcAge = function () {
+  //     console.log(2022 - this.birthYear);
+  //   };
+};
+
+const dave = new Person('Dave', 1995);
+console.log(dave);
+// 1. New {} is created
+// 2. func is called, this = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2010);
+const becham = new Person('Becham', 1992);
+console.log(matilda, becham);
+
+const jay = 'jay';
+console.log(dave instanceof Person);
+console.log(jay instanceof Person);
