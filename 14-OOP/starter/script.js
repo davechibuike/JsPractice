ab68ff1a458ca7c772b94e6c5f0474d82a3d9c32
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-11-08 00:59:51 -0800

    Es6 classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -27,7 +27,7 @@ const becham = new Person('becham', 1992);
 // console.log(jay instanceof Person);
 
 //* Prototypes
-console.log(Person.prototype);
+// console.log(Person.prototype);
 Person.prototype.calcAge = function () {
   console.log(2023 - this.birthYear);
 };
@@ -43,31 +43,90 @@ Person.prototype.upperFirstName = function () {
 // dave.upperFirstName();
 
 // * View all prototypes
-console.log(dave.__proto__);
-console.log(dave.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(dave));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(dave.__proto__);
+// console.log(dave.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(dave));
+// console.log(Person.prototype.isPrototypeOf(Person));
 //*Think of this as = .isPrototypeOfLinkedObject
 
 Person.prototype.species = 'Homo sapient'; //can be array['Homo sapient', 'Human'];
-console.log(dave.species, matilda.species);
+// console.log(dave.species, matilda.species);
 
-console.log(dave.hasOwnProperty('firstName'));
-console.log(dave.__proto__);
-console.log(dave.__proto__.__proto__); //This would bring the main object propreties
-console.log(dave.__proto__.__proto__.__proto__);
+// console.log(dave.hasOwnProperty('firstName'));
+// console.log(dave.__proto__);
+// console.log(dave.__proto__.__proto__); //This would bring the main object propreties
+// console.log(dave.__proto__.__proto__.__proto__);
 
-console.dir(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
 //? Example
 const arr = [2, 3, 4, 3, 4, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__);
+// console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
 
 //! Add a prototype to get all the unique values in an Array
 Array.prototype.unique = function () {
   return [...new Set(this)];
 };
 
-console.log(arr.unique());
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(h1.__proto__);
+
+// console.dir(x => x + 1);
+
+//! Coding challenge
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at ${this.speed} Km/h`);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed} Km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.accelerate();
+
+// bmw.break();
+// bmw.break();
+// bmw.break();
+// bmw.break();
+
+// mercedes.accelerate();
+// mercedes.break();
+
+// ES6 Class
+
+//*Class experssion
+// const PersonC = class {};
+
+//*Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
