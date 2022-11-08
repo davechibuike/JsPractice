ecff161c4d2e639d2bfa3645ab5322f05de931f9
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-11-08 06:50:21 -0800

    Code challenge

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -88,23 +88,23 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} going at ${this.speed} Km/h`);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} going at ${this.speed} Km/h`);
+// };
 
-Car.prototype.break = function () {
-  this.speed -= 5;
-  console.log(`${this.make} going at ${this.speed} Km/h`);
-};
+// Car.prototype.break = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} going at ${this.speed} Km/h`);
+// };
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
 
 // bmw.accelerate();
 // bmw.accelerate();
@@ -220,16 +220,64 @@ const PersonProto = {
 };
 
 const stephen = Object.create(PersonProto);
-console.log(stephen);
+// console.log(stephen);
 
 stephen.name = 'Stephen';
 stephen.birthYear = 1995;
-console.log(stephen);
-stephen.calcAge();
-console.log(stephen.__proto__);
-console.log(stephen.__proto__ === PersonProto);
+// console.log(stephen);
+// stephen.calcAge();
+// console.log(stephen.__proto__);
+// console.log(stephen.__proto__ === PersonProto);
 
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 2001);
-sarah.calcAge();
-console.log(sarah);
+// sarah.calcAge();
+// console.log(sarah);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+DATA CAR 1: 'Ford' going at 120 km/h
+GOOD LUCK 😀
+
+*/
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed} Km/h`);
+  }
+
+  break() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed} Km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    this.speed = speed * 1.6;
+    return this.speed;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford);
+ford.accelerate();
+ford.break();
+ford.break();
+console.log(ford.speedUS);
+ford.speedUs = 50;
+console.log(ford);
