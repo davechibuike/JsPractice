47be0431bcc6795ddc49cd0648494179b2d109b0
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-12-21 05:20:15 -0800

    AJAX call

--- async js/script.js
+++ async js/script.js
@@ -4,3 +4,40 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (...countries) {
+  for (const country of countries) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+
+      const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+              <h3 class="country__name">${Object.values(data.name)[0]}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                data.population / 1000000
+              ).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️</span>${Object.values(
+                data.languages
+              )}</p>
+              <p class="country__row"><span>💰</span>${
+                Object.values(data.currencies)[0].name
+              }</p>
+            </div>
+          </article>
+  
+    `;
+
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+      countriesContainer.style.opacity = 1;
+    });
+  }
+};
+
+getCountryData('portugal', 'usa');
