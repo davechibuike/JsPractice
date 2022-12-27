338bf0c15d81906865e2c512a9a57fa72dd178fe
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-12-27 05:46:36 -0800

    event loop practice

--- async js/script.js
+++ async js/script.js
@@ -342,3 +342,13 @@ const whereAmI = function (lat, lng) {
 // whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+// Event Loop in practive
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec Timer'), 0);
+// Promise.resolve('Resolved Promise 1').then(res => console.log(res));
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let x = 0; x < 1000000000; x++) {}
+//   console.log(res);
+// });
+// console.log('Test end');
