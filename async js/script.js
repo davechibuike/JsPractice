4a33c22eba463f93f10951024b980116c177649a
AnotherSoftwareCompany <chibike.dave@gmail.com>
2022-12-27 07:06:37 -0800

    event loop practice

--- async js/script.js
+++ async js/script.js
@@ -352,3 +352,39 @@ const whereAmI = function (lat, lng) {
 //   console.log(res);
 // });
 // console.log('Test end');
+
+// Building a Promise
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Live Draw Loading...');
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve('You Win 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
